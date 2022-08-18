f8e19156e54bd5d2ab56a74ebb860db9d4b491c0
Podduturi <npodduturi@deloitte.co.uk>
2022-08-18 19:00:06 +0100

    module

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ module "london" {
   providers = {
     aws = aws.london
   }
-  aws_region = "eu-west-2"
 }
 
 module "ireland" {
@@ -19,5 +18,4 @@ module "ireland" {
   providers = {
     aws = aws.ireland
   }
-  aws_region = "eu-west-1"
 }
