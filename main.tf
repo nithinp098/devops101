aafaf5d46d2a25e8c52c47ccd04281108173c573
Podduturi <npodduturi@deloitte.co.uk>
2022-08-19 11:11:41 +0100

    London

--- main.tf
+++ main.tf
@@ -16,15 +16,6 @@ module "london" {
   image_id = "ami-0e34bbddc66def5ac"
 }
 
-module "ireland" {
-  source     = "./modules/platform-modules"
-  providers = {
-    aws = aws.ireland
-  }
-  aws_region = "eu-west-1"
-  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  image_id = "ami-089950bc622d39ed8"
-}
 
 
 
